371cedb77b8fc03548e49dd6090ebc61c69d61cf
aimelive <aimendayambaje24@gmail.com>
2023-09-02 19:32:20 +0200

    changing cloudinary folder

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
 export class CloudinaryService {
-  private readonly FOLDER = 'MCSA-Student-Awards-Testing2';
+  private readonly FOLDER = process.env.CLOUDINARY_FOLDER;
 
   async uploadImage(image: string) {
     try {
